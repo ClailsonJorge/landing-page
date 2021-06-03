768cb0ad1898a21a14b626f922374060c420dd57
ClailsonJorge <clailsonjorge@gmail.com>
2021-06-03 20:19:41 -0300

    chore: class 69 create first query

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,12 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  query GET_LADING_PAGE {
+    ladingPage {
+      logo {
+        alternativeText
+        url
+      }
+    }
+  }
+`
+
+export default GET_LANDING_PAGE
